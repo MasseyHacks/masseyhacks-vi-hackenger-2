8888ba82fb738a0eb574dfe0b48bf25015b912b9
David Hui <opensource@davidhui.ca>
2020-05-06 22:44:25 -0400

    most questions added

---
+++ genSite.py
@@ -0,0 +1,6 @@
+with open("base.html", encoding="utf8") as b:
+    bP = b.read()
+    for i in range(6):
+        with open("page{}.html".format(i), encoding="utf8") as p:
+            with open("out/out{}.html".format(i), "w", encoding="utf8") as o:
+                o.write(bP.replace("{{innerPage}}", p.read()).replace("{{cdnBase}}", ""))
